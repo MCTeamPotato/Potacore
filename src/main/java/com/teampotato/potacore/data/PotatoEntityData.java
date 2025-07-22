c1d3b63f5dca382c892155866ec1d9333f1ff161
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-07-22 12:40:29 +0800

    make PotatoEntityData thread-safe

--- src/main/java/com/teampotato/potacore/data/PotatoEntityData.java
+++ src/main/java/com/teampotato/potacore/data/PotatoEntityData.java
@@ -2,8 +2,9 @@ package com.teampotato.potacore.data;
 
 import com.teampotato.potacore.Potacore;
 import com.teampotato.potacore.util.NBTUtils;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
-import net.minecraft.nbt.*;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.saveddata.SavedData;
 import org.jetbrains.annotations.NotNull;
@@ -11,9 +12,10 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PotatoEntityData extends SavedData {
-    private final Map<UUID, Map<String, Object>> entityData = new Object2ObjectOpenHashMap<>();
+    private final Map<UUID, Map<String, Object>> entityData = new ConcurrentHashMap<>();
 
     public static @NotNull PotatoEntityData get(@NotNull ServerLevel level) {
         return level.getDataStorage().computeIfAbsent(PotatoEntityData::load, PotatoEntityData::new, "potato_entity_data");
@@ -31,7 +33,7 @@ public class PotatoEntityData extends SavedData {
             try {
                 UUID uuid = UUID.fromString(uuidStr);
                 CompoundTag entityTag = tag.getCompound(uuidStr);
-                Map<String, Object> dataMap = new Object2ObjectOpenHashMap<>();
+                Map<String, Object> dataMap = new ConcurrentHashMap<>();
                 for (String key : entityTag.getAllKeys()) {
                     Object value = NBTUtils.fromTag(entityTag.get(key));
                     if (value != null) {
@@ -67,12 +69,12 @@ public class PotatoEntityData extends SavedData {
     }
 
     public void setData(@NotNull UUID entity, @NotNull String key, @NotNull Object value) {
-        entityData.computeIfAbsent(entity, u -> new Object2ObjectOpenHashMap<>()).put(key, value);
+        entityData.computeIfAbsent(entity, u -> new ConcurrentHashMap<>()).put(key, value);
         setDirty();
     }
 
     public void setDataMap(@NotNull UUID entity, @NotNull Map<String, Object> dataMap) {
-        entityData.put(entity, new Object2ObjectOpenHashMap<>(dataMap));
+        entityData.put(entity, new ConcurrentHashMap<>(dataMap));
         setDirty();
     }
 
@@ -91,7 +93,7 @@ public class PotatoEntityData extends SavedData {
     }
 
     public @NotNull Map<String, Object> getDataMap(@NotNull UUID uuid) {
-        return entityData.computeIfAbsent(uuid, u -> new Object2ObjectOpenHashMap<>());
+        return entityData.computeIfAbsent(uuid, u -> new ConcurrentHashMap<>());
     }
 
     public @Nullable Object getData(@NotNull UUID entity, @NotNull String key) {

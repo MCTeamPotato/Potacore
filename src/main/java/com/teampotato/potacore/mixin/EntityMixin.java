27656576593935c49ddc7b174ae64800ac718016
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-07-24 16:40:03 +0800

    that's not performant

--- src/main/java/com/teampotato/potacore/mixin/EntityMixin.java
+++ src/main/java/com/teampotato/potacore/mixin/EntityMixin.java
@@ -1,27 +1,20 @@
 package com.teampotato.potacore.mixin;
 
-import com.teampotato.potacore.data.EntitiesInChunkData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
     @Shadow private Level level;
-
-    @Shadow private ChunkPos chunkPosition;
 
     @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Mth;createInsecureUUID(Lnet/minecraft/util/RandomSource;)Ljava/util/UUID;"), require = 0)
     private UUID initId(RandomSource randomSource) {
@@ -32,21 +25,5 @@ public abstract class EntityMixin {
             }
         }
         return id;
-    }
-
-    @Inject(method = "setPosRaw", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ChunkPos;<init>(Lnet/minecraft/core/BlockPos;)V"))
-    private void chunkPosUpdatePre(double x, double y, double z, CallbackInfo ci) {
-        Entity self = (Entity) (Object) this;
-        if (self instanceof LivingEntity entity && entity.level() instanceof ServerLevel serverLevel) {
-            EntitiesInChunkData.removeEntity(entity, serverLevel);
-        }
-    }
-
-    @Inject(method = "setPosRaw", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ChunkPos;<init>(Lnet/minecraft/core/BlockPos;)V", shift = At.Shift.AFTER))
-    private void chunkPosUpdatePost(double x, double y, double z, CallbackInfo ci) {
-        Entity self = (Entity) (Object) this;
-        if (self instanceof LivingEntity entity && entity.level() instanceof ServerLevel serverLevel) {
-            EntitiesInChunkData.addEntity(serverLevel, entity);
-        }
     }
 }
